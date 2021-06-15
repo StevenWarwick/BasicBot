b6deb7525b4ff13f49799f35512a15d068bae8a0
steven warwick <sjw1995@swbell.net>
2021-06-15 00:48:54 -0500

    Add project files.

---
+++ BasicBot/Modules/Commands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+using CalendarQuickstart;
+
+namespace BasicBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        
+        [Command("test")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Connected");
+        }
+        [Command("CalTest")]
+        public async Task CalStart()
+        {
+            await ReplyAsync(CalendarQuickstart.CalendarEvents.Calender());
+        }
+    }
+}
